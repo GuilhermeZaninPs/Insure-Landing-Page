bcae4797464f68986737a7a4680d82737b0d7df5
Guilherme Zanin <guizanin224@gmail.com>
2022-05-03 23:28:35 +0100

    script commit

--- scripts.js
+++ scripts.js
@@ -2,6 +2,12 @@ const iconHamburger = document.querySelector('.icons-hamburger');
 const iconClose = document.querySelector('.icons-close')
 const linksNav = document.querySelector('.links-nav-mobile')
 
+const width = window.screen.width
+
+const imgMobileIntro = document.querySelector('.image-intro-mobile')
+const imgDesktopIntro = document.querySelector('.image-intro-desktop')
+
+
 iconHamburger.addEventListener("click", () => {
     linksNav.style.display = "flex"
     iconClose.style.display = "inherit"
@@ -12,3 +18,9 @@ iconClose.addEventListener("click", () => {
     iconClose.style.display = "none"
     linksNav.style.display = "none"
 })
+
+// if (width >= 940) {
+//     imgMobileIntro.style.display = "none"
+//     imgDesktopIntro.style.display = "flex"
+
+// }
